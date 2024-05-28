da1bcf91003f787fc0ee89ae1694f3ace566593a
Syifa'udin Ridho <starcreator.work@gmail.com@email.com>
2024-05-28 15:31:15 +0700

    Init Pailit

--- src/routes/(app)/[[slug]]/+page.svelte
+++ src/routes/(app)/[[slug]]/+page.svelte
@@ -67,7 +67,7 @@
 	</p>
 {:else}
 	<p class="mb-4 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-6">
-		Welcome to PKPU
+		Welcome to Pailit
 	</p>
 	<Card size="sm" padding="lg">
 		<form class="flex flex-col space-y-6" on:submit|preventDefault={handleSubmit}>
